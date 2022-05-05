db4f1bf3bfe80d8c38895392aaf1ad1fc1c92425
chrishoonS <chrishoon90@daum.net>
2022-05-05 23:14:05 +0900

    project 생성

---
+++ CodeStudy/src/test/java/Solution02Test.java
@@ -0,0 +1,13 @@
+import com.test.Solution02;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution02Test {
+    Solution02 slt02 = new Solution02();
+    @Test
+    public void test(){
+        assertEquals(slt02.solution(new int[]{1,2,3,4}, new int[]{-3,-1,0,2}), 3);
+        assertEquals(slt02.solution(new int[]{-1,0,1}, new int[]{1,0,-1}), -2);
+    }
+}
