1459a65c40592e1f9af3c3ebdcdfde8a190627cc
chrishoon <chrishoon90@daum.net>
2022-05-09 16:05:18 +0900

    feat : 부족금액 구하기 finish

---
+++ CodeStudy/src/main/java/com/test/Solution08.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Solution08 {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long sum = 0;
+        for(long i=1; i<=count; i++){
+            sum += (price * i);
+        }
+        answer = money - sum;
+        return (sum < money) ? 0 : Math.abs(answer); //answer가 양수면 0, 음수면 절대값
+    }
+}
