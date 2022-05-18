905b53a7764bc4661c64f545d1211aa02d096f51
chrishoon <chrishoon90@daum.net>
2022-05-18 14:41:57 +0900

    신고결과 받기 ing

---
+++ CodeStudy/src/main/java/com/test/Solution21.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Solution21 {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ CodeStudy/src/test/java/com/test/Solution21Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution21Test {
+
+    @Test
+    void solution() {
+        Solution21 slt21 = new Solution21();
+        assertArrayEquals(slt21.solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"},2), new int[]{2, 1, 1, 0});
+        assertArrayEquals(slt21.solution(new String[]{"con", "ryan"}, new String[]{"ryan con", "ryan con", "ryan con", "ryan con"},3), new int[]{0, 0});
+    }
+}
+
+
