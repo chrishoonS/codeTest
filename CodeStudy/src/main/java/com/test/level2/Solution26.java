bdf778eb677ad7e18b5056b2df89ddf539940184
chrishoon <chrishoon90@daum.net>
2022-05-23 16:17:22 +0900

    2개 이하로 다른 비트 ing

---
+++ CodeStudy/src/main/java/com/test/level2/Solution26.java
@@ -0,0 +1,8 @@
+package com.test.level2;
+
+public class Solution26 {
+    public long[] solution(long[] numbers) {
+        long[] answer = {};
+        return answer;
+    }
+}
---
+++ CodeStudy/src/test/java/com/test/level2/Solution26Test.java
@@ -0,0 +1,14 @@
+package com.test.level2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution26Test {
+
+    @Test
+    void solution() {
+        Solution26 slt = new Solution26();
+        assertArrayEquals(slt.solution(new long[]{2, 7}),new long[]{3, 11});
+    }
+}
