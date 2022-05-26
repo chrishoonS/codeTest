85ae7820b13dc326db50a262eca8dcb9009ff01b
chrishoon <chrishoon90@daum.net>
2022-05-26 11:23:13 +0900

    완주하지 못한 선수(HashMap으로 풀기) finish

--- CodeStudy/src/main/java/com/test2/level1/Algorithm01.java
+++ CodeStudy/src/main/java/com/test2/level1/Algorithm01.java
@@ -1,17 +1,20 @@
 package com.test2.level1;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Algorithm01 {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-        for (int i = 0; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i])){
-                return participant[i];
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+                break;
             }
         }
-        return participant[participant.length-1];
+        return answer;
     }
 }
