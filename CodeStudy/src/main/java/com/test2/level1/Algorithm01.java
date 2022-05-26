42e9beda5210abe305cc627c7fea0a48500f45cd
chrishoon <chrishoon90@daum.net>
2022-05-26 11:17:48 +0900

    완주하지 못한 선수 finish

--- CodeStudy/src/main/java/com/test2/level1/Algorithm01.java
+++ CodeStudy/src/main/java/com/test2/level1/Algorithm01.java
@@ -1,24 +1,17 @@
 package com.test2.level1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Algorithm01 {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
 
-        ArrayList<String> compleList = new ArrayList<>(Arrays.asList(completion));
-
-        for (int i = 0; i < participant.length; i++) {
-            if (compleList.indexOf(participant[i]) == -1){
-                answer = participant[i];
-                break;
-            }else{
-                compleList.remove(participant[i]);
+        for (int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
-        return answer;
+        return participant[participant.length-1];
     }
 }
