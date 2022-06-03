b979c56bec20561532473f61b900555f9d01cee6
chrishoon <chrishoon90@daum.net>
2022-06-03 15:40:50 +0900

    백준 1차원 배열 (1/7) ing

---
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main04.java
@@ -0,0 +1,22 @@
+package com.BaekjoonCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main04 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        in.close();
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N - 1]);
+    }
+}
