5c29c2996ee7fb00255d8c7f58382805017f16e5
chrishoon <chrishoon90@daum.net>
2022-06-08 10:42:00 +0900

    시저 암호 finish

---
+++ CodeStudy/src/main/java/com/test3/level1/Excercise14.java
@@ -0,0 +1,15 @@
+package com.test3.level1;
+
+public class Excercise14 {
+    public String solution(String s, int n) {
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            //대소문자 판별 -> ASCII code 연산 -> char casting
+            if (Character.isLowerCase(ch)) ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            else if (Character.isUpperCase(ch)) ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            answer += ch;
+        }
+        return answer;
+    }
+}
---
+++ CodeStudy/src/test/java/com/test3/level1/Excercise14Test.java
@@ -0,0 +1,16 @@
+package com.test3.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Excercise14Test {
+
+    @Test
+    void solution() {
+        Excercise14 ex = new Excercise14();
+        assertEquals(ex.solution("AB",1),"BC");
+        assertEquals(ex.solution("z",1),"a");
+        assertEquals(ex.solution("a B z",4),"e F d");
+    }
+}
