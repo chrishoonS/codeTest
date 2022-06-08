e08fc8e30c02cafde10b2c456a4e4336072e026e
chrishoon <chrishoon90@daum.net>
2022-06-08 14:09:39 +0900

    자연수 뒤집어 배열로 만들기 finish

---
+++ CodeStudy/src/test/java/com/test3/level1/Excercise18Test.java
@@ -0,0 +1,14 @@
+package com.test3.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Excercise18Test {
+
+    @Test
+    void solution() {
+        Excercise18 ex = new Excercise18();
+        assertArrayEquals(ex.solution(12345),new int[]{5,4,3,2,1});
+    }
+}
