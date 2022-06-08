cb9c38e4f1482d3d2e0cfd7cb764bb1aa558e5c4
chrishoon <chrishoon90@daum.net>
2022-06-08 10:01:11 +0900

    문자열을 정수로 바꾸기 finish

---
+++ CodeStudy/src/test/java/com/test3/level1/Excercise13Test.java
@@ -0,0 +1,15 @@
+package com.test3.level1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Excercise13Test {
+
+    @Test
+    void solution() {
+        Excercise13 ex = new Excercise13();
+        assertEquals(ex.solution("1234"), 1234);
+        assertEquals(ex.solution("-1234"), -1234);
+    }
+}
