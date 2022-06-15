8d3416ee5c6c48b581f23b61c4ad0a501be9498b
chrishoon <chrishoon90@daum.net>
2022-06-15 09:52:54 +0900

    백준 1712 ing (1/8) finish

--- CodeStudy/src/main/java/com/BaekjoonCode/Main07.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main07.java
@@ -6,21 +6,11 @@ import java.util.Scanner;
 public class Main07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long fixAmt = sc.nextLong();   //고정비용 3
-        long varAmt = sc.nextLong();   //가변비용 2
-        long price = sc.nextLong();   //책정가 1
+        int fixAmt = sc.nextInt();   //고정비용
+        int varAmt = sc.nextInt();   //가변비용
+        int price = sc.nextInt();   //책정가
 
-        long prdtAmt = 0;
-        int cnt = 0;
-        //생산비용 계산 및 손익분기점 개수 구하기
-        do{
-            if(varAmt > price){
-                cnt = -1;
-                break;
-            }
-            cnt++; //물품 판매개수 1
-            prdtAmt = fixAmt + varAmt*cnt; //3+2 = 5
-        }while(prdtAmt >= price*cnt);
-        System.out.println(cnt);
+        if (price <= varAmt) System.out.println(-1);
+        else System.out.println((fixAmt / (price-varAmt)) + 1);
     }
 }
