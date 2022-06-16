3a3e8a7b352fa09e100ac7733fd7e1bf2a856215
chrishoon <chrishoon90@daum.net>
2022-06-16 11:09:47 +0900

    Stream 소스코드 해석

--- CodeStudy/src/main/java/com/test/level1/Solution12.java
+++ CodeStudy/src/main/java/com/test/level1/Solution12.java
@@ -1,14 +1,29 @@
 package com.test.level1;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class Solution12 {
+//    public int solution(int[] nums) {
+//        int answer = 0;
+//        HashSet<Integer> poketSet = new HashSet<>();
+//        for(int i : nums){ //중복제거
+//            poketSet.add(i);
+//        }
+//        answer = (poketSet.size() >= nums.length / 2) ? nums.length / 2 : poketSet.size();
+//        return answer;
+//    }
+
     public int solution(int[] nums) {
-        int answer = 0;
-        HashSet<Integer> poketSet = new HashSet<>();
-        for(int i : nums){ //중복제거
-            poketSet.add(i);
-        }
-        answer = (poketSet.size() >= nums.length / 2) ? nums.length / 2 : poketSet.size();
-        return answer;
+        /**
+         * nums 배열을 받아 Arrays.stream(nums).boxed로 스트림을 생성한뒤,
+         * Collectors.collectingAndThen로 Set으로 만들고,
+         * phonekemons로 Set을 입력받아 phonkemons Set의 사이즈와 num.length/2의 최소값을 구해서,
+         * Set의 int값 return
+         **/
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
     }
 }
