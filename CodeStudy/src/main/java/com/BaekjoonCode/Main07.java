4ea161d7a64adbda36c12563446dc5b7754f23c6
chrishoon <chrishoon90@daum.net>
2022-06-20 18:00:46 +0900

    백준 기본수학1 2839 (7/8) ing

--- CodeStudy/src/main/java/com/BaekjoonCode/Main07.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main07.java
@@ -3,7 +3,6 @@ package com.BaekjoonCode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 //1712
 //public class Main07 {
@@ -69,21 +68,98 @@ import java.util.StringTokenizer;
 //}
 
 //2869
+//public class Main07 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//        int A = Integer.parseInt(st.nextToken());
+//        int B = Integer.parseInt(st.nextToken());
+//        int V = Integer.parseInt(st.nextToken());
+//        int day = (V-B) / (A-B);
+//        if ((V-B) % (A-B) != 0) day++;
+//
+//        System.out.println(day);
+//    }
+//}
+
+//10250
+//public class Main07 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int T = Integer.parseInt(br.readLine());
+//        int[] ho = new int[T];
+//        for (int i = 0; i < T; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            int H = Integer.parseInt(st.nextToken());   //높이
+//            int W = Integer.parseInt(st.nextToken());   //층
+//            int N = Integer.parseInt(st.nextToken());   //N번째 손님
+//
+//            if(N % H == 0) System.out.println((H*100) + (N / H));
+//            else System.out.println(((N % H) * 100) + ((N / H) + 1));
+//        }
+//    }
+//}
+
+//2775
+//public class Main07 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        /**
+//         * a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다
+//         * k층 n호에 몇명 사는지 구하기!!!
+//         * 0층엔 1호부터 있고 i호에 i명이 산다
+//         * 2 /1 3/ 2 3
+//         * 1층 1호 = 1
+//         * 1층 2호 = 1+2 = 3
+//         * 1층 3호 = 1+2+3 = 6
+//         * 2층 1호 = 1
+//         * 2층 2호 = 1+3 = 4
+//         * 2층 3호 = 1+3+6 = 10
+//         **/
+//
+//
+//        int[][] arr = new int[15][15];
+//
+//        for (int i = 0; i < arr.length ; i++) {
+//            arr[i][1] = 1;
+//            arr[0][i] = i;
+//        }
+//
+//        for (int i = 1; i < 15; i++) {
+//            for (int j = 2; j < 15; j++) {
+//                arr[i][j] = arr[i][j-1] + arr[i-1][j];
+//            }
+//        }
+//
+//        int T = Integer.parseInt(br.readLine());
+//
+//        for (int i = 0; i < T; i++) {
+//            int k = Integer.parseInt(br.readLine());
+//            int n = Integer.parseInt(br.readLine());
+//            System.out.println(arr[k][n]);
+//        }
+//    }
+//}
+
+
+//2839
 public class Main07 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        int quot = 0; //몫
+        int rmd = 0; //나머지
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-        int day = (V-B) / (A-B);
-        if ((V-B) % (A-B) != 0) day++;
+        for (int i = 5; i >= 3; i--) {
+            if(i==4) continue;
+            else {
+                quot = quot + (N / i);
+                rmd = N % i;
+                if (rmd % i == rmd) continue;
+            }
+        }
+        System.out.println(quot);
 
-        System.out.println(day);
     }
 }
-
-//10250 <- 여기서부터 풀기
-
-//2775
