1a11b94ef1648d9a2844209b6ccbe67c6fa56f8c
chrishoon <chrishoon90@daum.net>
2022-06-22 11:29:04 +0900

    비밀지도 코드 복습

--- CodeStudy/src/main/java/com/test/level1/Solution16.java
+++ CodeStudy/src/main/java/com/test/level1/Solution16.java
@@ -5,7 +5,7 @@ public class Solution16 {
         String[] answer = new String[n];
         //십진수 -> 이진수
         for (int i=0; i<n; i++) {
-            //이진수로 변환시켜 연산 후 길이 변환을 위해(?)long으로 타입 변환
+            //이진수로 변환시켜 연산 후, int 범위를 벗어나는 경우를 막아주기위해 long으로 타입 변환
             answer[i] = String.format("%0"+n+"d",Long.parseLong(Integer.toBinaryString(arr1[i] | arr2[i])));
             answer[i] = answer[i].replace("0", " ");
             answer[i] = answer[i].replace("1", "#");
