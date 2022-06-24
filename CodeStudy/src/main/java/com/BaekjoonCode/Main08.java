7f1cd27e430b645fae880497d8966a1f596b154f
chrishoon <chrishoon90@daum.net>
2022-06-24 18:05:44 +0900

    백준 기본수학2 11653 (2/6) ing

--- CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
@@ -33,6 +33,51 @@ import java.io.InputStreamReader;
 //}
 
 //2581
+//public class Main08 {
+//    public static boolean prime[];
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int M = Integer.parseInt(br.readLine());
+//        int N = Integer.parseInt(br.readLine());
+//
+//        prime = new boolean[N + 1];	// 배열 생성
+//        get_prime();
+//
+//        // 소수 합 및 최솟값
+//        int sum = 0;
+//        int min = Integer.MAX_VALUE;
+//        for(int i = M; i <= N; i++) {
+//            if(prime[i] == false) {	// false = 소수
+//                sum += i;
+//                if(min == Integer.MAX_VALUE) {	// 첫 소수가 최솟값임
+//                    min = i;
+//                }
+//            }
+//        }
+//        if(sum == 0) {	// 소수가 없다면
+//            System.out.println(-1);
+//        }else {
+//            System.out.println(sum);
+//            System.out.println(min);
+//        }
+//    }
+//
+//    public static void get_prime() {
+//        prime[0] = true;
+//        prime[1] = true;
+//
+//        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+//            if(prime[i]) continue;	// 이미 체크된 배열일 경우 skip
+//            for(int j = i * i; j < prime.length; j += i) {
+//                prime[j] = true;
+//            }
+//        }
+//    }
+//}
+
+//11653
 public class Main08 {
     public static boolean prime[];
 
