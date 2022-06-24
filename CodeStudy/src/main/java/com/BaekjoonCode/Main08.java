c53b958146d28b245d8a3e55e37043956b7497d6
chrishoon <chrishoon90@daum.net>
2022-06-24 10:38:58 +0900

    백준 기본수학2 2581 (2/8) finish

--- CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
@@ -3,32 +3,76 @@ package com.BaekjoonCode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 //10757
-public class Main08 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//public class Main08 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//
+//        int cnt = 0;
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//        for (int i = 0; i < N; i++) {
+//            int a = Integer.parseInt(st.nextToken());
+//            if(a == 1) continue;
+//            else{
+//                if(chkSosu(a) == true) cnt++;
+//            }
+//        }
+//        System.out.println(cnt);
+//    }
+//
+//    private static boolean chkSosu(int a){
+//        for (int i = 2; i < a; i++) {
+//            if(a%i == 0) return false;
+//        }
+//        return true;
+//    }
+//}
 
+//2581
+public class Main08 {
+    public static boolean prime[];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        prime = new boolean[N + 1];	// 배열 생성
+        get_prime();
 
-        for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(st.nextToken());
-            if(a == 1) continue;
-            else{
-                if(chkSosu(a) == true) cnt++;
+        // 소수 합 및 최솟값
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = M; i <= N; i++) {
+            if(prime[i] == false) {	// false = 소수
+                sum += i;
+                if(min == Integer.MAX_VALUE) {	// 첫 소수가 최솟값임
+                    min = i;
+                }
             }
         }
-        System.out.println(cnt);
+        if(sum == 0) {	// 소수가 없다면
+            System.out.println(-1);
+        }else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
     }
 
-    private static boolean chkSosu(int a){
-        for (int i = 2; i < a; i++) {
-            if(a%i == 0) return false;
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;	// 이미 체크된 배열일 경우 skip
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
         }
-        return true;
     }
 }
