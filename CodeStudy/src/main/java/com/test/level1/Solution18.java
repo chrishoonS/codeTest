8546ac349396eba2a0140585cb74cfbe551f610a
chrishoon <chrishoon90@daum.net>
2022-06-28 17:14:08 +0900

    실패율 코드 리뷰

--- CodeStudy/src/main/java/com/test/level1/Solution18.java
+++ CodeStudy/src/main/java/com/test/level1/Solution18.java
@@ -1,66 +1,68 @@
 package com.test.level1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Solution18 {
-    public int[] solution(int N, int[] stages) {
-        /**
-         * 스테이지 수 만큼 반복
-         * 실패율 계산
-         * 배열에 각 스테이지별 실패율과 해당 스테이지 담기
-         * 담긴 배열의 순서 정렬
-         **/
+class Solution18 {
+    public int[] solution(int N, int[] lastStages) {
+        int nPlayers = lastStages.length;
+        int[] nStagePlayers = new int[N + 2];
+        //nStagePlayers 배열에 각 스테이지별 플레이어 수 저장
+        for (int stage : lastStages) {
+            nStagePlayers[stage] += 1;
+        }
+
+        int remainingPlayers = nPlayers;
+        List<Stage> stages = new ArrayList<>();
+        for (int id = 1 ; id <= N; id++) {
+            double failure = (double) nStagePlayers[id] / remainingPlayers;
+            remainingPlayers -= nStagePlayers[id];
+
+            //Stage 클래스객체에 해당 stage와 실패율을 리스트에 저장
+            Stage s = new Stage(id, failure);
+            stages.add(s);
+        }
+        //내림차순으로 정렬
+        Collections.sort(stages, Collections.reverseOrder());
+
         int[] answer = new int[N];
-        double[] failArr = new double[N];
-        for (int i=0; i<N; i++) {               // 스테이지 수 N만큼 반복
-            double clearCnt = 0;                //스테이지 도달한 플레이어 수 - 분모
-            double ingCnt = 0;                  //스테이지 중인 플레이어 수 - 분자
-            for (int j=0; j<stages.length; j++) {
-                if(i+1 == stages[j]){           //i+1번째 스테이지 깻는지 체크하여 분모/분자 증가
-                    ingCnt++;
-                    clearCnt++;
-                }else if (i+1 < stages[j]){
-                    clearCnt++;
-                }
-            }
-            if(clearCnt != 0){                  //분모가 0일때는 연산하지 않고 0값 넣기
-                failArr[i] = ingCnt / clearCnt;
-                answer[i] = i+1;
-            }else{
-                failArr[i] = 0;
-                answer[i] = i+1;
-            }
+        for (int i = 0; i < N; i++) {
+            answer[i] = stages.get(i).id;
         }
-//        System.out.println(Arrays.toString(answer));
-//        System.out.println(Arrays.toString(failArr));
-        double tmp = 0;     //실패율 배열 값을 담을 임시 변수
-        int stgTmp = 0;     //해당 스테이지 값을 담을 임시 변수
-
-        for (int i=0; i<failArr.length; i++) {
-            for (int j=i+1; j<failArr.length; j++) {
-                if (failArr[i] > failArr[j]){
-                    continue;
-                }else if(failArr[i] == failArr[j]){
-                    if(answer[i] < answer[j]){
-                        continue;
-                    }else{
-                        stgTmp = answer[i];
-                        answer[i] = answer[j];
-                        answer[j] = stgTmp;
-                    }
-                }else{
-                    tmp = failArr[i];
-                    failArr[i] = failArr[j];
-                    failArr[j] = tmp;
-                    stgTmp = answer[i];
-                    answer[i] = answer[j];
-                    answer[j] = stgTmp;
-                }
-            }
-        }
-//        System.out.println(Arrays.toString(answer));
-//        System.out.println(Arrays.toString(failArr));
-
         return answer;
+    }
+
+    /**
+     * 내부 클래스란 클래스 안에 선언된 클래스
+     * 클래스 안에 인스턴스 변수, 즉 필드를 선언하는 위치에 선언되는 경우. 보통 중첩클래스 혹은 인스턴스 클래스라고 한다
+     * 세번째로는 메소드 안에 클래스를 선언한 경우, 지역 중첩 클래스 또는 지역 클래스라고 한다. 메소드 안에서 해당 클래스를 이용할 수 있다.
+     */
+    class Stage implements Comparable<Stage> {
+        public int id;
+        public double failure;
+
+        public Stage(int id_, double failure_) {
+            id = id_;
+            failure = failure_;
+        }
+
+        /**
+         * @Override : 자바 컴파일러에게 부모 클래스로부터 메소드 오버라이딩을 하겠다는 메시지 전달
+         * 반환형, 메소드 이름, 매개 변수가 모두 같은 오버라이딩 가능한 메소드가 없다면 잘못 되었다고 알려줌.
+         * 자바에서 메소드 오버라이딩이란, 상속의 관계에 있는 클래스 간에 하위 클래스가 상위 클래스와 '완전 동일한 메소드'를 덮어쓴다는 의미이다.
+         * 여기서 '완전 동일한 메소드'라는 말은 이름과 반환형이 같으면서 매개변수의 개수와 타입까지 모두 같은 메소드라는 의미
+         * Comparable 클래스의 compareTo 메서드는 매개변수로 Generic변수를 받지만 @Override를 사용하여 Stage 클래스객체를 매개변수로 받아 사용
+         **/
+        @Override
+        public int compareTo(Stage o) {
+            if (failure < o.failure ) {
+                return -1;
+            }
+            if (failure > o.failure ) {
+                return 1;
+            }
+            return 0;
+        }
     }
 }
--- CodeStudy/src/test/java/com/test/level1/Solution18Test.java
+++ CodeStudy/src/test/java/com/test/level1/Solution18Test.java
@@ -9,7 +9,7 @@ class Solution18Test {
     @Test
     void solution() {
         Solution18 slt18 = new Solution18();
-//        assertEquals(slt18.solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3}), new int[]{3, 4, 2, 1, 5});
-        assertEquals(slt18.solution(4, new int[]{4, 4, 4, 4, 4}), new int[]{4,1,2,3});
+        assertArrayEquals(slt18.solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3}), new int[]{3, 4, 2, 1, 5});
+        assertArrayEquals(slt18.solution(4, new int[]{4, 4, 4, 4, 4}), new int[]{4,1,2,3});
     }
 }
