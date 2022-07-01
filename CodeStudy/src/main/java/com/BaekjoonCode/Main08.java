5fb96675a207eb8cb94436eb79869773b0323336
chrishoon <chrishoon90@daum.net>
2022-07-01 17:26:38 +0900

    백준 기본수학2 4948 (5/6) ing

--- CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
@@ -124,6 +124,30 @@ import java.util.StringTokenizer;
 //    }
 //}
 
+//public class Main08 {
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//        int M = Integer.parseInt(st.nextToken());
+//        int N = Integer.parseInt(st.nextToken());
+//
+//        boolean[] prime = new boolean[N + 1];
+//
+//        for(int i = 2; i <= N; i++) {
+//            if(prime[i]) continue;
+//            if(i >= M) sb.append(i).append('\n');
+//            for(int j = i + i; j <= N; j += i) {
+//                prime[j] = true;
+//            }
+//        }
+//        System.out.println(sb);
+//    }
+//}
+
+//4948
 public class Main08 {
     public static void main(String[] args) throws IOException {
 
@@ -133,16 +157,5 @@ public class Main08 {
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-
-        boolean[] prime = new boolean[N + 1];
-
-        for(int i = 2; i <= N; i++) {
-            if(prime[i]) continue;
-            if(i >= M) sb.append(i).append('\n');
-            for(int j = i + i; j <= N; j += i) {
-                prime[j] = true;
-            }
-        }
-        System.out.println(sb);
     }
 }
