8cc5dce11c1ebf62c915bbaf2ab5700797f29fa0
chrishoon <chrishoon90@daum.net>
2022-07-05 17:26:48 +0900

    백준 기본수학2 4948 (5/6) finish

--- CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
@@ -3,7 +3,6 @@ package com.BaekjoonCode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 //10757
 //public class Main08 {
@@ -149,13 +148,35 @@ import java.util.StringTokenizer;
 
 //4948
 public class Main08 {
+    public static boolean[] prime = new boolean[246912];
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        get_prime();	// 소수를 얻는 메소드 실행
 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        while(true){
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0) break;
+
+            int cnt = 0;
+            for (int i = n+1; i < 2 * n; i++) {
+                if(!prime[i]) cnt++;
+            }
+            sb.append(cnt).append('\n');
+        }
+        System.out.print(sb);
     }
+
+    public static void get_prime() {
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+
 }
