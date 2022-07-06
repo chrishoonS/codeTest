bee9a5ba404c68e4691875569f6db27bc6f7093d
chrishoon <chrishoon90@daum.net>
2022-07-06 17:49:55 +0900

    백준 기본수학2 9020 (6/6) finish

--- CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main08.java
@@ -147,23 +147,70 @@ import java.io.InputStreamReader;
 //}
 
 //4948
+//public class Main08 {
+//
+//    public static boolean[] prime = new boolean[246912];
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        get_prime();	// 소수를 얻는 메소드 실행
+//
+//        while(true){
+//            int n = Integer.parseInt(br.readLine());
+//            if(n == 0) break;
+//
+//            int cnt = 0;
+//            for (int i = n+1; i < 2 * n; i++) {
+//                if(!prime[i]) cnt++;
+//            }
+//            sb.append(cnt).append('\n');
+//        }
+//        System.out.print(sb);
+//    }
+//
+//    public static void get_prime() {
+//        prime[0] = prime[1] = true;
+//
+//        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+//            if(prime[i]) continue;
+//            for(int j = i * i; j < prime.length; j += i) {
+//                prime[j] = true;
+//            }
+//        }
+//    }
+//
+//}
+
+//9020
 public class Main08 {
-    public static boolean[] prime = new boolean[246912];
+    public static boolean[] prime = new boolean[10001];
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        get_prime();	// 소수를 얻는 메소드 실행
 
-        while(true){
+        get_prime();
+
+        int T = Integer.parseInt(br.readLine());	// 테스트케이스
+
+        while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            if(n == 0) break;
+            int first_partition = n / 2;
+            int second_partition = n / 2;
 
-            int cnt = 0;
-            for (int i = n+1; i < 2 * n; i++) {
-                if(!prime[i]) cnt++;
+            while (true) {
+
+                // 두 파티션이 모두 소수일 경우
+                if (!prime[first_partition] && !prime[second_partition]) {
+                    sb.append(first_partition).append(' ').append(second_partition).append('\n');
+                    break;
+                }
+                first_partition--;
+                second_partition++;
             }
-            sb.append(cnt).append('\n');
         }
         System.out.print(sb);
     }
@@ -178,5 +225,4 @@ public class Main08 {
             }
         }
     }
-
 }
