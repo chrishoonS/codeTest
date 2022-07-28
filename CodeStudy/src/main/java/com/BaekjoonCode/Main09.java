a08f32cbfb787a62f4394c7bae5bf27c43ba9564
chrishoon <chrishoon90@daum.net>
2022-07-28 17:16:12 +0900

    백준 17478 재귀(3/5) 2

--- CodeStudy/src/main/java/com/BaekjoonCode/Main09.java
+++ CodeStudy/src/main/java/com/BaekjoonCode/Main09.java
@@ -1,8 +1,6 @@
 package com.BaekjoonCode;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 //10872
 //public class Main09 {
@@ -44,35 +42,52 @@ import java.io.InputStreamReader;
 
 //17478
 public class Main09 {
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        int count = Integer.parseInt(br.readLine());
+        bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n" );
+        recursive(count, count);
 
-        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-
-        recur(0, "");
-
+        br.close();
+        bw.close();
     }
 
-    public static void recur(int depth, String under) {
-        if (depth == n) {
-            System.out.println(under + "\"재귀함수가 뭔가요?\"");
-            System.out.println(under + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-            System.out.println(under + "라고 답변하였지.");
-            return;
+    static void recursive(int num, int k) throws IOException {
+        if (k >= 0) {
+            for (int i = num - k; i > 0; i--) {
+                bw.write("____" );
+            }
+            bw.write("\"재귀함수가 뭔가요?\"\n" );
 
+            if (k == 0) {
+                for (int i = num - k; i > 0; i--) {
+                    bw.write("____" );
+                }
+                bw.write("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n" );
+            }
+
+            else {
+                for (int i = num - k; i > 0; i--) {
+                    bw.write("____" );
+                }
+                bw.write("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" );
+                for (int i = num - k; i > 0; i--) {
+                    bw.write("____" );
+                }
+                bw.write("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" );
+                for (int i = num - k; i > 0; i--) {
+                    bw.write("____" );
+                }
+                bw.write("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n" );
+            }
+            recursive(num, --k);
+            for (int i = num - k; i > 1; i--) {
+                bw.write("____" );
+            }
+            bw.write("라고 답변하였지.\n" );
         }
-
-        System.out.println(under + "\"재귀함수가 뭔가요?\"");
-        System.out.println(under + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-        System.out.println(under + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-        System.out.println(under + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-
-
-        recur(depth + 1, under + "____");
-        System.out.println(under + "라고 답변하였지.");
     }
 }
