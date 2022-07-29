bc73dad57ce46483176e0675153eb266ee334b91
chrishoon <chrishoon90@daum.net>
2022-07-29 12:57:52 +0900

    study Object-Oriented Programming

--- CodeStudy/src/main/java/com/test/level1/Solution21.java
+++ CodeStudy/src/main/java/com/test/level1/Solution21.java
@@ -33,7 +33,7 @@ public class Solution21 {
         return answer;
     }*/
 
-    public int[] solution(String[] id_list, String[] report, int k) {
+    /*public int[] solution(String[] id_list, String[] report, int k) {
         List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
         HashMap<String, Integer> count = new HashMap<>();
         for (String s : list) {
@@ -46,5 +46,50 @@ public class Solution21 {
             List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
             return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
         }).mapToInt(Long::intValue).toArray();
+    }*/
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        class User{
+            String name;
+            HashSet<String> reportList;
+            HashSet<String> reportedList;
+            public User(String name){
+                this.name = name;
+                reportList = new HashSet<>();
+                reportedList = new HashSet<>();
+            }
+        }
+
+        int[] answer = new int[id_list.length];
+        ArrayList<User> users = new ArrayList<>();
+        HashMap<String,Integer> suspendedList = new HashMap<>(); //<이름>
+        HashMap<String,Integer> idIdx = new HashMap<String,Integer>(); // <이름, 해당 이름의 User 클래스 idx>
+        int idx = 0;
+
+        for(String name : id_list) {
+            idIdx.put(name, idx++);
+            users.add(new User(name));
+        }
+
+        for(String re : report){
+            String[] str = re.split(" ");
+            users.get( idIdx.get(str[0])).reportList.add(str[1]);
+            users.get( idIdx.get(str[1])).reportedList.add(str[0]);
+        }
+
+        for(User user : users){
+            if(user.reportedList.size() >= k)
+                suspendedList.put(user.name,1);
+        }
+
+        for(User user : users){
+            for(String nameReport : user.reportList){
+                if(suspendedList.get(nameReport) != null){
+                    answer[idIdx.get(user.name)]++;
+                }
+
+            }
+        }
+        return answer;
     }
 }
