8853c32a4a80c04666c40b35c208d1199bae7c67
song <chrishoon90@daum.net>
2022-11-09 17:21:43 +0900

    java throw study

---
+++ CodeStudy/src/main/java/com/javaExample/throwEx/ExceptionStudy.java
@@ -0,0 +1,15 @@
+package com.javaExample.throwEx;
+
+import java.io.IOException;
+
+public class ExceptionStudy {
+    public static void main(String[] args){
+        try {
+            throw new IOException();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("try/catch 끝나고 예외 처리 완료");
+        }
+    }
+}
