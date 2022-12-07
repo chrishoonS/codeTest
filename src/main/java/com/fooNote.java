bd0bad39d4c8f46c3cdbb4ffb7054d1ce01bc2d3
song <chrishoon90@daum.net>
2022-12-07 19:00:09 +0900

    Use getOrDefault(k,v) to map null check

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -2,17 +2,21 @@ package com;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 public class fooNote {
     public static void main(String[] args) {
-        String str = "2022.12.12";
-//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        if (str != null && !str.isEmpty()) {
-            formatter.setTimeZone(TimeZone.getTimeZone(str));
-        }
-        formatter.format(new Date());
-        System.out.println(formatter.format(new Date()));
+        Map<String, Object> map = new HashMap<>();
+        map.put("test1", 123);
+        map.put("test2", 234);
+        map.put("test3", 345);
+
+        System.out.println(map.containsKey("test1"));
+        System.out.println(map.containsKey("test2"));
+        System.out.println(map.containsKey("test4"));
+        System.out.println(map.get("test5"));
+        System.out.println(map.getOrDefault("test5", 123123123));
     }
 }
