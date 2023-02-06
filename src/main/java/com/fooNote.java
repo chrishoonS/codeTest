f68161c192d7c64cf904af8650a486b99ffa9545
song <chrishoon90@daum.net>
2023-02-06 11:16:16 +0900

    java 코드 시간 측정

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -1,35 +1,18 @@
 package com;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Duration;
+import java.time.Instant;
 
 public class fooNote {
     public static void main(String[] args) {
+        Instant start = Instant.now();
 
-        String date1 = "20230119";
-        String date2 = "20230118";
-
-        DateFormat format = new SimpleDateFormat("yyyyMMdd");
-
-        /* Date타입으로 변경 */
-
-        Date d1 = null;
-        try {
-            d1 = format.parse(date1);
-            Date d2 = format.parse(date2);
-            long Sec = (d1.getTime() - d2.getTime()) / 1000; // 초
-//            long Min = (d1.getTime() - d2.getTime()) / 60000; // 분
-//            long Hour = (d1.getTime() - d2.getTime()) / 3600000; // 시
-            long Days = Sec / (24 * 60 * 60); // 일자수
-
-            System.out.println(Sec + "초 차이");
-//            System.out.println(Min + "분 차이");
-//            System.out.println(Hour + "시 차이");
-            System.out.println(Days + "일 차이");
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
+        for (long i = 0; i < 1000000000; i++) {
+            long a = i * i;
         }
+
+        Instant finish = Instant.now();
+        long terms = Duration.between(start, finish).toMillis();
+        System.out.println("시간차이(ms) : " + terms);
     }
 }
