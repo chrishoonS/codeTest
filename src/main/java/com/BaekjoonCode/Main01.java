3b24b88c9b9ba1c4de2f9a464dedd99c4f6df64f
song <chrishoon90@daum.net>
2023-02-15 16:11:55 +0900

    백준 10172, 3003 추가

--- src/main/java/com/BaekjoonCode/Main01.java
+++ src/main/java/com/BaekjoonCode/Main01.java
@@ -1,37 +1,16 @@
 package com.BaekjoonCode;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main01 {
 //    [2557]
 //    public static void main(String[] args){
 //        System.out.println("Hello World!");
-//    }
-
-//    [10718]
-//    public static void main(String[] args){
-//        System.out.println("강한친구 대한육군");
-//        System.out.println("강한친구 대한육군");
-//    }
-
-//    [10171]
-//    public static void main(String args[]){
-//        System.out.println("\\    /\\\n" +
-//                " )  ( \')\n"+
-//                "(  /  )\n"+
-//                " \\(__)|\n");
-//    }
-
-//    [25083]
-//    public static void main(String[] args) {
-//        System.out.println("         ,r'\"7\n" +
-//                "r`-_   ,'  ,/\n" +
-//                " \\. \". L_r'\n" +
-//                "   `~\\/\n" +
-//                "      |\n" +
-//                "      |");
-//
 //    }
 
 //    [1000]
@@ -42,7 +21,7 @@ public class Main01 {
 //        b = sc.nextInt();
 //        System.out.println(a + b);
 //    }
-//
+
 //    [1001]
 //    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
@@ -51,7 +30,7 @@ public class Main01 {
 //        b = sc.nextInt();
 //        System.out.println(a - b);
 //    }
-//
+
 //    [10998]
 //    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
@@ -60,7 +39,7 @@ public class Main01 {
 //        b = sc.nextInt();
 //        System.out.println(a * b);
 //    }
-//
+
 //    [1008]
 //    public class Main {
 //        public static void main(String[] args) {
@@ -71,7 +50,7 @@ public class Main01 {
 //            System.out.println(a / b);
 //        }
 //    }
-//
+
 //    [10869]
 //    public class Main {
 //        public static void main(String[] args) {
@@ -105,7 +84,34 @@ public class Main01 {
 //            System.out.println(y - 543);
 //        }
 //    }
+
+// [3003]
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //
+//        int king = 1;
+//        int queen = 1;
+//        int rook = 2;
+//        int bishop = 2;
+//        int knight = 2;
+//        int pawn = 8;
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        king = king - Integer.parseInt(st.nextToken());
+//        queen = queen - Integer.parseInt(st.nextToken());
+//        rook = rook - Integer.parseInt(st.nextToken());
+//        bishop = bishop - Integer.parseInt(st.nextToken());
+//        knight = knight - Integer.parseInt(st.nextToken());
+//        pawn = pawn - Integer.parseInt(st.nextToken());
+//
+//        System.out.print(king + " ");
+//        System.out.print(queen + " ");
+//        System.out.print(rook + " ");
+//        System.out.print(bishop + " ");
+//        System.out.print(knight + " ");
+//        System.out.print(pawn + " ");
+//    }
+
 //    [10430]
 //    public class Main {
 //        public static void main(String[] args) {
@@ -122,17 +128,47 @@ public class Main01 {
 //        }
 //    }
 
-//    [2588]
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int a, b;
-            a = sc.nextInt();
-            b = sc.nextInt();
-            String s = String.valueOf(b);
 
-            for (int i = 0; i < s.length(); i++) {
-                System.out.println(a * Integer.parseInt(s.substring(s.length()-i-1, s.length()-i)));
-            }
-            System.out.println(a * b);
-        }
+//    [2588]
+//        public static void main(String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            int a, b;
+//            a = sc.nextInt();
+//            b = sc.nextInt();
+//            String s = String.valueOf(b);
+//
+//            for (int i = 0; i < s.length(); i++) {
+//                System.out.println(a * Integer.parseInt(s.substring(s.length()-i-1, s.length()-i)));
+//            }
+//            System.out.println(a * b);
+//        }
+
+
+//    [10171]
+//    public static void main(String args[]){
+//        System.out.println("\\    /\\\n" +
+//                " )  ( \')\n"+
+//                "(  /  )\n"+
+//                " \\(__)|\n");
+//    }
+
+//    [10172]
+//    public static void main(String args[]){
+//            System.out.println("|\\_/|\n" +
+//                    "|q p|   /}\n" +
+//                    "( 0 )\"\"\"\\\n" +
+//                    "|\"^\"`    |\n" +
+//                    "||_/=\\\\__|");
+//    }
+
+//    [25083]
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+    }
 }
