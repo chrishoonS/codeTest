bc7c606ab20711ae704fb29cc462730b30adc334
song <chrishoon90@daum.net>
2023-03-09 16:43:47 +0900

    백준 25304, 25314 추가

--- src/main/java/com/BaekjoonCode/Main03.java
+++ src/main/java/com/BaekjoonCode/Main03.java
@@ -3,6 +3,7 @@ package com.BaekjoonCode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 //2739
 //public class Main03 {
@@ -42,6 +43,45 @@ import java.io.InputStreamReader;
 //        System.out.println(a*(a+1)/2);
 //    }
 //}
+
+//25304
+//public class Main03 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//
+//        int X = Integer.parseInt(br.readLine());
+//        int N = Integer.parseInt(br.readLine());
+//
+//        int total = 0;
+//        for(int i = 0; i < N; i++) {
+//            st = new StringTokenizer(br.readLine());
+//            total += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+//        }
+//        if(X == total)
+//            System.out.println("Yes");
+//        else
+//            System.out.println("No");
+//    }
+
+//25314
+public class Main03 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n/4; i++) {
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+    }
+
+
+
 
 //15552
 //public class Main03 {
@@ -318,31 +358,31 @@ import java.io.InputStreamReader;
 //        System.out.println(cycle);
 //    }
 //}
-public class Main03 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-        int N_tmp = N; // N 복사
-        int N_first; // 첫째 자리
-        int N_second; // 둘째 자리
-        int N_sum; // 첫째 자리 + 둘째 자리
-        int cnt = 0;
-
-        // 처음 N_tmp와 N이 같기때문에
-        // do-while문을 써준다.
-        do{
-            // ex.) 26
-            cnt++;
-            N_first = N_tmp % 10; // N_first : 6
-            N_second = N_tmp / 10; // N_second : 2
-            N_sum = N_first + N_second; // N_sum : 8
-
-            N_second = N_first; // N_second : 6
-            N_first = N_sum % 10; // N_first : 8
-            N_tmp = N_second * 10 + N_first; // N_tmp : 68
-        }while(N_tmp != N);
-
-        System.out.println(cnt);
-    }
+//public class Main03 {
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int N = Integer.parseInt(br.readLine());
+//        int N_tmp = N; // N 복사
+//        int N_first; // 첫째 자리
+//        int N_second; // 둘째 자리
+//        int N_sum; // 첫째 자리 + 둘째 자리
+//        int cnt = 0;
+//
+//        // 처음 N_tmp와 N이 같기때문에
+//        // do-while문을 써준다.
+//        do{
+//            // ex.) 26
+//            cnt++;
+//            N_first = N_tmp % 10; // N_first : 6
+//            N_second = N_tmp / 10; // N_second : 2
+//            N_sum = N_first + N_second; // N_sum : 8
+//
+//            N_second = N_first; // N_second : 6
+//            N_first = N_sum % 10; // N_first : 8
+//            N_tmp = N_second * 10 + N_first; // N_tmp : 68
+//        }while(N_tmp != N);
+//
+//        System.out.println(cnt);
+//    }
 }
