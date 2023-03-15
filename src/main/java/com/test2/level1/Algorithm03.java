f5216fd9eed598d3a93094a192396f900dadc1f0
song <chrishoon90@daum.net>
2023-03-15 16:03:54 +0900

    supo 변수명 수정

--- src/main/java/com/test2/level1/Algorithm03.java
+++ src/main/java/com/test2/level1/Algorithm03.java
@@ -5,18 +5,21 @@ import java.util.Collections;
 
 public class Algorithm03 {
     public int[] solution(int[] answers) {
-        int[] _1supo = {1, 2, 3, 4, 5};
-        int[] _2supo = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] _3supo = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] supo1 = {1, 2, 3, 4, 5};
+        int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
         int a=0, b=0, c=0;
+
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i % answers.length] == _1supo[i % _1supo.length]) a++;
-            if (answers[i % answers.length] == _2supo[i % _2supo.length]) b++;
-            if (answers[i % answers.length] == _3supo[i % _3supo.length]) c++;
+            if (answers[i % answers.length] == supo1[i % supo1.length]) a++;
+            if (answers[i % answers.length] == supo2[i % supo2.length]) b++;
+            if (answers[i % answers.length] == supo3[i % supo3.length]) c++;
         }
+
         int maxVal = Math.max(Math.max(a,b), c);
         ArrayList<Integer> ansList = new ArrayList<>();
+
         if (maxVal == a) ansList.add(1);
         if (maxVal == b) ansList.add(2);
         if (maxVal == c) ansList.add(3);
