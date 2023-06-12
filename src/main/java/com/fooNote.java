300166a01acd777331e4764d3f6db9440796c545
song <chrishoon90@daum.net>
2023-06-12 13:19:58 +0900

    소수점 자리 테스트

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -7,12 +7,19 @@ public class fooNote {
     public static void main(String[] args) {
         Instant start = Instant.now();
 
-        double a = 1.256;
+        String a = "1.3";
+        String b = "1.3";
 
 
         Instant finish = Instant.now();
         long terms = Duration.between(start, finish).toMillis();
         System.out.println("시간차이(ms) : " + terms);
-        System.out.println(String.valueOf(a).substring(0,3));
+        System.out.println(Double.parseDouble(a));
+        System.out.println(Double.parseDouble(b));
+        if(Double.parseDouble(a) > Double.parseDouble(b)){
+            System.out.println("a > b");
+        }else{
+            System.out.println("a <= b");
+        }
     }
 }
