44bda70bc5b3225457bbb0232cea8d7a74304789
song <chrishoon90@daum.net>
2023-07-18 15:23:58 +0900

    백준 10998 풀이 완료

--- src/main/java/com/BaekjoonCode/Main06.java
+++ src/main/java/com/BaekjoonCode/Main06.java
@@ -3,7 +3,6 @@ package com.BaekjoonCode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 //    [25083] 새싹
 //    public static void main(String[] args) {
@@ -16,40 +15,95 @@ import java.util.StringTokenizer;
 //
 //    }
 
-// [2444] 별 찍기 - 7
-public class Main06 {
-    public static void main(String[] args) throws IOException {BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++) {
-                sb.append(" ");
-            }
-            for (int j = 1; j <= 2*i-1; j++) {
-                sb.append("*");
-            }
-            sb.append("\n");
-        }
-        for (int i = n-1; i >= 1; i--) {
-            for (int j = 1; j <= n-i; j++) {
-                sb.append(" ");
-            }
-            for (int j = 1; j <= 2*i-1; j++) {
-                sb.append("*");
-            }
-            sb.append("\n");
-        }
-        System.out.print(sb);
-    }
-}
-
-//11654
-//public class Main06 {
+// [3003]
 //    public static void main(String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        System.out.println((br.readLine()).charAt(0)-0);
+//
+//        int king = 1;
+//        int queen = 1;
+//        int rook = 2;
+//        int bishop = 2;
+//        int knight = 2;
+//        int pawn = 8;
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//        king = king - Integer.parseInt(st.nextToken());
+//        queen = queen - Integer.parseInt(st.nextToken());
+//        rook = rook - Integer.parseInt(st.nextToken());
+//        bishop = bishop - Integer.parseInt(st.nextToken());
+//        knight = knight - Integer.parseInt(st.nextToken());
+//        pawn = pawn - Integer.parseInt(st.nextToken());
+//
+//        System.out.print(king + " ");
+//        System.out.print(queen + " ");
+//        System.out.print(rook + " ");
+//        System.out.print(bishop + " ");
+//        System.out.print(knight + " ");
+//        System.out.print(pawn + " ");
+//    }
+
+// [2444] 별 찍기 - 7
+//public class Main06 {
+//    public static void main(String[] args) throws IOException {BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int n = Integer.parseInt(br.readLine());
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n-i; j++) {
+//                sb.append(" ");
+//            }
+//            for (int j = 1; j <= 2*i-1; j++) {
+//                sb.append("*");
+//            }
+//            sb.append("\n");
+//        }
+//        for (int i = n-1; i >= 1; i--) {
+//            for (int j = 1; j <= n-i; j++) {
+//                sb.append(" ");
+//            }
+//            for (int j = 1; j <= 2*i-1; j++) {
+//                sb.append("*");
+//            }
+//            sb.append("\n");
+//        }
+//        System.out.print(sb);
 //    }
 //}
+
+//10998 팰린드롬인지 확인하기
+public class Main06 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] input = br.readLine().toCharArray();
+        int cnt = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if(input[i] != input[input.length-1-i])
+                break;
+            else cnt++;
+        }
+
+        if (cnt == input.length) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        br.close();
+
+        // 좀더 빠른 다른 사람 소스
+//        for( int i = 0; i < str.length/2; i++) {
+//            if( !(str[i] == str[str.length-1-i])) {
+//                System.out.println(0);
+//                return;
+//            }
+//        }
+//
+//        System.out.println(1);
+
+    }
+}
 
 
 //1157
