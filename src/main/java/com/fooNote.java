3b601c633c1226e1394929b48bc97ae68292d182
song <chrishoon90@daum.net>
2023-08-07 17:54:54 +0900

    비트연산자, System.in.read() 공부

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -1,23 +1,35 @@
 package com;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
+import java.io.IOException;
 
 public class fooNote {
-    public static void main(String[] args) {
-        Function(5);
-    }
+    public static void main(String[] args) throws IOException {
+        int w = read();
+        int x = read();
+        int y = read();
+        int z = read();
 
-    public static void Function(int num) {
-        if(num == 0)
-            return;
-        else {
-            System.out.println("반갑습니다");
-            Function(num - 1);
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+    static int read() throws IOException {
+        int c;
+        int n = System.in.read() & 15;
+
+        n = (n << 3);
+        System.out.println("n1 ::: " + n);
+        n = (n << 1);
+        System.out.println("n2 ::: " + n);
+
+        while ((c = System.in.read()) > 32) {
+
+            n = (c & 15);
+            System.out.println("n3 ::: " + n);
+            n = (n << 3) + (n << 1) + (c & 15);
+            System.out.println("n4 ::: " + n);
         }
+        return n;
     }
 }
