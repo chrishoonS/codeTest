7aae9577df494fba2d6bd63c1accf59f7173d39b
song <chrishoon90@daum.net>
2023-10-30 14:33:46 +0900

    HttpServletRequest test

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -1,47 +1,96 @@
 package com;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.SecureRandom;
 import java.util.Date;
 
-public class fooNote {
+@WebServlet(name="requestHeaderServlet", urlPatterns = "/request-header")
+public class fooNote extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        printStartLine(req);
+//        printHeaders(req);
+//        printHeaderUtils(req);
+        printEtc(req);
+
+        resp.getWriter().write("ok");
+    }
+
+    private void printStartLine(HttpServletRequest req) {
+        System.out.println("--- REQUEST-LINE - start ---");
+        System.out.println("req.getMethod() = " + req.getMethod());                 // GET
+        System.out.println("req.getProtocol() = " + req.getProtocol());             // HTTP/1.1
+        System.out.println("req.getScheme() = " + req.getScheme());                 // http
+        System.out.println("req.getRequestURL() = " + req.getRequestURL());         // http://localhost:8080/request-header
+        System.out.println("req.getRequestURI() = " + req.getRequestURI());         // request-header
+        System.out.println("req.getQueryString() = " + req.getQueryString());       // username=hello
+        System.out.println("req.isSecure() = " + req.isSecure());                   // https 사용 유무(false)
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+//    private void printHeaders(HttpServletRequest req) {
+//        System.out.println("--- Headers - start ---");
+//        req.getHeaderNames().asIterator()
+//                .forEachRemaining(headerName -> System.out.println(headerName + ": " + req.getHeader(headerName)));
+//        System.out.println("--- Headers - end ---");
+//        System.out.println();
+//    }
+//
+//    private void printHeaderUtils(HttpServletRequest req) {
+//        System.out.println("--- Headers 편의 조회 start ---");
+//        System.out.println("[Host 편의 조회]");
+//        System.out.println("req.getServerName() = " + req.getServerName());
+//        System.out.println("req.getServerPort() = " + req.getServerPort());
+//        System.out.println();
+//
+//        System.out.println("[Accept-Language 편의 조회]");
+//        req.getLocales().asIterator()
+//                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+//        System.out.println("req.getLocale() = " + req.getLocale());
+//        System.out.println();
+//
+//        System.out.println("[cookie 편의 조회]");
+//        if(req.getCookies() != null) {
+//            for (Cookie cookie : req.getCookies()) {
+//                System.out.println(cookie.getName() + ": " + cookie.getValue());
+//            }
+//        }
+//        System.out.println();
+//
+//        System.out.println("[Content 편의 조회]");
+//        System.out.println("req.getContentType() = " + req.getContentType());
+//        System.out.println("req.getCharacterEncoding() = " + req.getCharacterEncoding());
+//
+//        System.out.println("--- Headers 편의 조회 end ---");
+//        System.out.println();
+//    }
+
+    private void printEtc(HttpServletRequest req) {
+        System.out.println("--- 기타 조회 start ---");
+        System.out.println("[Remote 정보]");
+        System.out.println("req.getRemoteHost() = " + req.getRemoteHost());
+        System.out.println("req.getRemoteAddr() = " + req.getRemoteAddr());
+        System.out.println("req.getRemotePort() = " + req.getRemotePort());
+        System.out.println();
+
+        System.out.println("[Locale 정보]");
+        System.out.println("req.getLocalName() = " + req.getLocalName());
+        System.out.println("req.getLocalAddr() = " + req.getLocalAddr());
+        System.out.println("req.getLocalPort() = " + req.getLocalPort());
+
+        System.out.println("--- 기타 조회 end ---");
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        // 출처 : https://byul91oh.tistory.com/466
-        System.out.println(getRandomPassword(6));
-    }
 
-    public static String getRandomPassword(int size) {
-        char[] charSet = new char[] {
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                '!', '@', '#', '$', '%', '^', '&' };
-
-        StringBuffer sb = new StringBuffer();
-        SecureRandom sr = new SecureRandom();
-        sr.setSeed(new Date().getTime());
-
-        int idx = 0;
-        int len = charSet.length;
-        for (int i=0; i<size; i++) {
-            // idx = (int) (len * Math.random());
-            idx = sr.nextInt(len);    // 강력한 난수를 발생시키기 위해 SecureRandom을 사용한다.
-            sb.append(charSet[idx]);
-        }
-
-        return sb.toString();
-    }
-
-    public static void httpServletTest(HttpServletRequest request){
-        System.out.println("=====START-LINE=====begin");
-        System.out.println("request.getMethod() = " + request.getMethod());
-        System.out.println("request.getProtocol() = " + request.getProtocol());
-        System.out.println("request.getRequestURI() = " + request.getRequestURI());
-        System.out.println("request.getRequestURL() = " + request.getRequestURL());
-        System.out.println("request.getScheme() = " + request.getScheme());
-        System.out.println("request.getQueryString() = " + request.getQueryString());
-        System.out.println("request.isSecure() = " + request.isSecure());
-        System.out.println("=====START-LINE=====end");
     }
 }
