79072c0df349254042cec1f56500e5634635922d
song <chrishoon90@daum.net>
2023-10-31 11:22:11 +0900

    @Slf4j 사용 TEST 및 날짜 비교

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -1,96 +1,41 @@
 package com;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.security.SecureRandom;
+import jdk.jfr.internal.JVM;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
-@WebServlet(name="requestHeaderServlet", urlPatterns = "/request-header")
-public class fooNote extends HttpServlet {
-
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        printStartLine(req);
-//        printHeaders(req);
-//        printHeaderUtils(req);
-        printEtc(req);
-
-        resp.getWriter().write("ok");
-    }
-
-    private void printStartLine(HttpServletRequest req) {
-        System.out.println("--- REQUEST-LINE - start ---");
-        System.out.println("req.getMethod() = " + req.getMethod());                 // GET
-        System.out.println("req.getProtocol() = " + req.getProtocol());             // HTTP/1.1
-        System.out.println("req.getScheme() = " + req.getScheme());                 // http
-        System.out.println("req.getRequestURL() = " + req.getRequestURL());         // http://localhost:8080/request-header
-        System.out.println("req.getRequestURI() = " + req.getRequestURI());         // request-header
-        System.out.println("req.getQueryString() = " + req.getQueryString());       // username=hello
-        System.out.println("req.isSecure() = " + req.isSecure());                   // https 사용 유무(false)
-        System.out.println("--- REQUEST-LINE - end ---");
-        System.out.println();
-    }
-
-//    private void printHeaders(HttpServletRequest req) {
-//        System.out.println("--- Headers - start ---");
-//        req.getHeaderNames().asIterator()
-//                .forEachRemaining(headerName -> System.out.println(headerName + ": " + req.getHeader(headerName)));
-//        System.out.println("--- Headers - end ---");
-//        System.out.println();
-//    }
-//
-//    private void printHeaderUtils(HttpServletRequest req) {
-//        System.out.println("--- Headers 편의 조회 start ---");
-//        System.out.println("[Host 편의 조회]");
-//        System.out.println("req.getServerName() = " + req.getServerName());
-//        System.out.println("req.getServerPort() = " + req.getServerPort());
-//        System.out.println();
-//
-//        System.out.println("[Accept-Language 편의 조회]");
-//        req.getLocales().asIterator()
-//                .forEachRemaining(locale -> System.out.println("locale = " + locale));
-//        System.out.println("req.getLocale() = " + req.getLocale());
-//        System.out.println();
-//
-//        System.out.println("[cookie 편의 조회]");
-//        if(req.getCookies() != null) {
-//            for (Cookie cookie : req.getCookies()) {
-//                System.out.println(cookie.getName() + ": " + cookie.getValue());
-//            }
-//        }
-//        System.out.println();
-//
-//        System.out.println("[Content 편의 조회]");
-//        System.out.println("req.getContentType() = " + req.getContentType());
-//        System.out.println("req.getCharacterEncoding() = " + req.getCharacterEncoding());
-//
-//        System.out.println("--- Headers 편의 조회 end ---");
-//        System.out.println();
-//    }
-
-    private void printEtc(HttpServletRequest req) {
-        System.out.println("--- 기타 조회 start ---");
-        System.out.println("[Remote 정보]");
-        System.out.println("req.getRemoteHost() = " + req.getRemoteHost());
-        System.out.println("req.getRemoteAddr() = " + req.getRemoteAddr());
-        System.out.println("req.getRemotePort() = " + req.getRemotePort());
-        System.out.println();
-
-        System.out.println("[Locale 정보]");
-        System.out.println("req.getLocalName() = " + req.getLocalName());
-        System.out.println("req.getLocalAddr() = " + req.getLocalAddr());
-        System.out.println("req.getLocalPort() = " + req.getLocalPort());
-
-        System.out.println("--- 기타 조회 end ---");
-        System.out.println();
-    }
-
+@Slf4j
+public class fooNote {
     public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dtfFull = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String currentDate = now.format(dtfFull).substring(0, 10);
+
+        log.debug(String.valueOf(now));
+        log.debug(String.valueOf(dtfFull));
+        log.debug(currentDate);
+
+        String totalStr = "20231031140000";
+        String dateStr = totalStr.substring(0, 4) +"-"+ totalStr.substring(4, 6) +"-"+ totalStr.substring(6, 8) +" "+
+                         totalStr.substring(8,10) +":"+ totalStr.substring(10,12) +":"+ totalStr.substring(12,14);
+        log.debug(dateStr);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        try {
+            Date date = formatter.parse(dateStr);
+            Date nowDate = new Date();
+            log.debug(String.valueOf(date));
+            log.debug(String.valueOf(date.after(nowDate)));
+            log.debug(String.valueOf(date.before(nowDate)));
+        } catch (Exception e) {
+            log.error("errororor:::::{}", e);
+        }
 
     }
 }
