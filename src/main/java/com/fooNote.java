da0cdb897e9eebb0f35acb48cd8ed8737b596293
chrishoon <hooney1103@gmail.com>
2025-07-30 10:52:24 +0900

    test code

--- src/main/java/com/fooNote.java
+++ src/main/java/com/fooNote.java
@@ -1,19 +1,18 @@
 package com;
 
+
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Slf4j
 public class fooNote {
 
     public static void main(String[] args) {
-        String[] arr1 = { "1", "2", "3", "4" };
-        String[] arr2 = { "2", "3", "5" };
+        String[] eventLoungeArr = { "1", "4"};
+        String[] eventLoungeArrBk = { "1", "2", "3"};
 
-        compareEventLoungeArrays(arr1, arr2);
+        compareEventLoungeArrays(eventLoungeArr, eventLoungeArrBk);
     }
 
     public static void compareEventLoungeArrays(String[] eventLoungeArr, String[] eventLoungeArrBk) {
@@ -25,12 +24,19 @@ public class fooNote {
             Set<String> diff = new HashSet<>(setA);
             diff.removeAll(setB);
             System.out.println("eventLoungeArr에만 있는 값: " + diff);
+            System.out.println("eventLoungeArr에 있는 값: " + setA);
+            System.out.println("eventLoungeArrBk에 있는 값: " + setB);
 
         } else if (eventLoungeArr.length < eventLoungeArrBk.length) {
             // B가 더 긴 경우 → B에만 있는 값
             Set<String> diff = new HashSet<>(setB);
+            List<String> list = new ArrayList<>();
+            list.addAll(diff);
+            System.out.println("TEST :::::  " + list);
             diff.removeAll(setA);
             System.out.println("eventLoungeArrBk에만 있는 값: " + diff);
+            System.out.println("eventLoungeArrBk에 있는 값: " + setB);
+            System.out.println("eventLoungeArr에 있는 값: " + setA);
 
         } else {
             // 길이는 같지만 내용이 다름
